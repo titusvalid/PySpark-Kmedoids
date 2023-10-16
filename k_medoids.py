68fd54c0eb6b5dec46ddc1c56a638fdb6e7b430c
nope <67336905+titusvalid@users.noreply.github.com>
2023-10-17 00:36:22 +0200

    Add files via upload

--- k_medoids.py
+++ k_medoids.py
@@ -1,3 +1,7 @@
+'''
+Light Weight K-Medoids Spark Implementation by Chus
+'''
+
 from pyspark import SparkContext, RDD
 from pyspark.sql import SparkSession
 import numpy as np
@@ -13,53 +17,56 @@ def find_new_center(cluster_points):
     cluster_list = list(cluster_points)
     return min(cluster_list, key=lambda x: np.sum([example_distance_func(x, y) for y in cluster_list]))
 
-def kmedoids_run_spark(rdd: RDD, n_clusters, dist_func, max_iter=1000, tol=0.001, sc=None):
-    print("kmedoids_run_spark")
-    indexed_rdd = rdd.filter(lambda x: len(x) > 0).zipWithIndex().map(lambda x: (x[1], x[0]))
 
-    print("Sample of indexed_rdd after zipWithIndex:", indexed_rdd.take(10))
-    
+
+
+
+
+
+def kmedoids_run_spark(rdd: RDD, n_clusters, dist_func, max_iter, tol, sc=None):
+  
+    indexed_rdd = rdd.filter(lambda x: len(x) > 0).zipWithIndex().map(lambda x: (x[1], x[0]))
     n_samples = indexed_rdd.count()
-    print("Number of samples:", n_samples)
+    print(f"Debug: Number of samples: {n_samples}")
     
-    centers_idx = np.random.choice(n_samples, n_clusters, replace=False).tolist()
-    print("Center indices:", centers_idx)
+    step = n_samples // n_clusters
+    centers_idx = [step * i for i in range(n_clusters)]
+    print(f"Debug: Initial Center indices: {centers_idx}")
 
     centers_data = indexed_rdd.filter(lambda x: x[0] in centers_idx).collect()
     centers_data = [x[1] for x in centers_data]
-    print("Initial Centers:", centers_data)
+    print(f"Debug: Initial Centers: {centers_data}")
+    
     broadcast_centers = sc.broadcast(centers_data)
+    
+    for current_iter in range(1, max_iter + 1):
+        print(f"Debug: Iteration {current_iter} of {max_iter}")
 
-    empty_rdd = indexed_rdd.filter(lambda x: len(x[1]) == 0)
-    print("Empty RDD count:", empty_rdd.count())
-
-    print("kmedoids_run_spark 2")
-    for _ in range(max_iter):
         cost_rdd = indexed_rdd.map(lambda x: (x[0], min([(i, dist_func(x[1], center)) for i, center in enumerate(broadcast_centers.value)], key=lambda t: t[1])))
-        print("kmedoids_run_spark 3")
+        sample_data = indexed_rdd.takeSample(False, 5)  # Take 5 random samples
+        for idx, data in sample_data:
+            distances = [dist_func(data, center) for center in broadcast_centers.value]
+            print(f"Debug: Sample {idx} distances to centers: {distances}")
+        
         new_centers_data = []
         for i in range(n_clusters):
             cluster_rdd = cost_rdd.filter(lambda x: x[1][0] == i).map(lambda x: x[0])
-            print("cluster_rdd:")
-            print("Count of cluster_rdd:", cluster_rdd.count())
-            print("Elements in cost_rdd:", cost_rdd.take(10))
-            print("Elements in cluster_rdd:", cluster_rdd.take(10))
-            print("Sample of indexed_rdd:", indexed_rdd.take(10))
+            cluster_count = cluster_rdd.count()
+            print(f"Debug: Cluster {i} size: {cluster_count}")
+
+            if cluster_count == 0:
+                print(f"Debug: Cluster {i} is empty. Skipping.")
+                continue
 
             pair_cluster_rdd = cluster_rdd.map(lambda x: (x, None))
             cluster_data = indexed_rdd.join(pair_cluster_rdd).map(lambda x: x[1][0])
-
-
-            cluster_data.foreach(print)
-            print("kmedoids_run_spark 4")
-            first_five = cluster_data.collect()[:10]
-            print("First 50 cluster_data:", first_five)
 
             sum_cluster_data = cluster_data.reduce(lambda x, y: np.array(x) + np.array(y))
             new_center = cluster_data.reduce(lambda a, b: a if dist_func(a, sum_cluster_data) < dist_func(b, sum_cluster_data) else b)
             new_centers_data.append(new_center)
 
         if all(dist_func(a, b) <= tol for a, b in zip(centers_data, new_centers_data)):
+            print("Debug: Convergence reached.")
             break
 
         centers_data = new_centers_data
@@ -69,8 +76,11 @@ def kmedoids_run_spark(rdd: RDD, n_clusters, dist_func, max_iter=1000, tol=0.001
     
     return centers_data, labeled_rdd
 
+
+
 class SparkKMedoids:
-    def __init__(self, n_clusters, dist_func=example_distance_func, max_iter=1000, tol=0.001, sc=None):
+    #set your own tolerance and iteration
+    def __init__(self, n_clusters, dist_func=example_distance_func, max_iter=25, tol=0.1, sc=None):
         print("SparkKMedoids")
         self.n_clusters = n_clusters
         self.dist_func = dist_func
@@ -86,3 +96,21 @@ class SparkKMedoids:
         raise NotImplementedError()
         
 
+    '''
+    Main API of KMedoids Clustering
+
+    Parameters
+    --------
+        n_clusters: number of clusters
+        dist_func : distance function
+        max_iter: maximum number of iterations
+        tol: tolerance
+
+
+    Methods
+    -------
+        fit(X): fit the model
+            - X: 2-D numpy array, size = (n_sample, n_features)
+
+        predict(X): predict cluster id given a test dataset.
+    '''
